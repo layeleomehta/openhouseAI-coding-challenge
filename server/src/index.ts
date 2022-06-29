8a22da63f564b32f51efa8d69d784e1e4f78792e
Laye Mehta <layeamehta@gmail.com>
2022-06-29 17:21:04 -0600

    created bad request and route not found custom errors, and added error handling middleware to index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express, { NextFunction, Request, Response } from "express";
 import { json } from "body-parser"; 
 import { submitLogRouter } from "./routes/submit-log";
 import { retrieveLogsRouter } from "./routes/retrieve-logs";
+import { errorHandler } from "./middlewares/error-handler";
 const PORT = 3000; 
 
 const app = express(); 
@@ -9,6 +10,8 @@ app.use(json());
 
 app.use(submitLogRouter); 
 app.use(retrieveLogsRouter); 
+
+app.use(errorHandler); 
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`); 
