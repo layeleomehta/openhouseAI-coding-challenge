e9bb7b956c9d958a031a691a243bce11b74b9307
Laye Mehta <layeamehta@gmail.com>
2022-06-29 14:08:32 -0600

    able to parse sample log data from submit-log route, created db.sql file for table creation later on

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,21 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import { json } from "body-parser"; 
 const PORT = 3000; 
 
 const app = express(); 
 app.use(json()); 
+
+app.post("/api/v1/submit-log", (req: Request, res: Response) => {
+    const userId = req.body.userId; 
+    const sessionId = req.body.sessionId; 
+    const actions = req.body.actions; 
+
+    console.log(actions); 
+    res.json("Hey"); 
+
+}); 
+
+app.get("/api/v1/retrieve-logs", () => {}); 
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`); 
